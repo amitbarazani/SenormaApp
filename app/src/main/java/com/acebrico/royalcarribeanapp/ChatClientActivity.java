7af750811936323bd357ca0f6e4f023b67aea486
cchenmmichaeli <amitbar111092@gmail.com>
2021-05-01 20:07:32 +0300

    - Added chat client activity - updated profile client activity

---
+++ app/src/main/java/com/acebrico/royalcarribeanapp/ChatClientActivity.java
@@ -0,0 +1,14 @@
+package com.acebrico.royalcarribeanapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_client);
+    }
+}
